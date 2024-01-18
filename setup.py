757b9d4f48c9d86a38c0a7d82d4b240c4a04c886
David Revay <daverevay@gmail.com>
2024-01-18 11:48:03 +1100

    chore: fix stuff

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup
 
+package_name = "adafruit_matrix_driver"
+
 setup(
-    name='adafruit_matrix_driver',
-    version='0.0.0',
-    packages=['adafruit_matrix_driver'],
-    # py_modules=['adafruit_matrix_driver'],
-    install_requires=['setuptools'],
-    author='Aiden Baker-Gabb',
-    author_email='Aiden@todo.com',
-    keywords=['ROS'],
-    classifiers=[],
-    description='Driver for Adafruit LED matrices',
-    license='Apache License, Version 2.0',
-    test_suite='test',
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Aiden Baker-Gabb",
+    maintainer_email="aidenbakergabb@gmail.com",
+    description="A ROS2 driver for the Adafruit 32x32 RGB LED Matrix",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             'adafruit_matrix_driver_node = adafruit_matrix_driver.adafruit_matrix_driver_node:main',
             'image_publisher_node = adafruit_matrix_driver.image_publisher_node:main'
         ],
